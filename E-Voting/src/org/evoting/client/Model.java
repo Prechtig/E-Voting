5d1a57baadb771b4c82a38d78653fafd4b2e75dd
Andreas Precht Poulsen <dresen_precht@hotmail.com>
2014-03-14 16:13:35 +0100

    removed unused imports

--- E-Voting/src/org/evoting/client/Model.java
+++ E-Voting/src/org/evoting/client/Model.java
@@ -1,9 +1,6 @@
 package org.evoting.client;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.BitSet;
-
 import org.evoting.client.exceptions.NoCandidateListException;
 
 public class Model
