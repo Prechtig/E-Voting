42fc74d59612a1a5030383997d5ec1bb5fb26179
Mikkel <mikkel.funch@gmail.com>
2014-03-24 11:38:05 +0100

    ISecurity interface has an implementation with ElGamal, RSA and SHA1

--- E-Voting/src/org/evoting/security/RSA.java
+++ E-Voting/src/org/evoting/security/RSA.java
@@ -1,109 +1,113 @@
 package org.evoting.security;
 
-import java.math.BigInteger;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.SecureRandom;
-import java.security.Signature;
+import java.security.PublicKey;
 
 import javax.crypto.Cipher;
-    
 
 public class RSA {
-   private final static BigInteger one      = new BigInteger("1");
-   private final static SecureRandom random = new SecureRandom();
+	private PublicKey pubK;
+	private PrivateKey privK;
+	
+	
+	public void encryptDecryptTest() throws SecurityException{
+		String m = "Test String";
+		
+		
+		//Generate keys
+		try {
+			KeyPairGenerator keyGen;
+			keyGen = KeyPairGenerator.getInstance("RSA");
+	
+			keyGen.initialize(1024);
+			//Provider p = keyGen.getProvider(); TODO: What is this for
+			
+			KeyPair keys = keyGen.generateKeyPair();
+			
+			pubK = keys.getPublic();
+			privK = keys.getPrivate();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-   private BigInteger privateKey;
-   private BigInteger publicKey;
-   private BigInteger modulus;
-
-   // generate an N-bit (roughly) public and private key
-   RSA(int N) {
-      BigInteger p = BigInteger.probablePrime(N/2, random);
-      BigInteger q = BigInteger.probablePrime(N/2, random);
-      BigInteger phi = (p.subtract(one)).multiply(q.subtract(one));
-
-      modulus    = p.multiply(q);                                  
-      publicKey  = new BigInteger("65537");     // common value in practice = 2^16 + 1
-      privateKey = publicKey.modInverse(phi);
-   }
-
-
-   BigInteger encrypt(BigInteger message) {
-      return message.modPow(publicKey, modulus);
-   }
-
-   BigInteger decrypt(BigInteger encrypted) {
-      return encrypted.modPow(privateKey, modulus);
-   }
-
-   public String toString() {
-      String s = "";
-      s += "public  = " + publicKey  + "\n";
-      s += "private = " + privateKey + "\n";
-      s += "modulus = " + modulus;
-      return s;
-   }
- 
-   public static void main(String[] args) {
-      int N = Integer.parseInt(args[0]);
-      RSA key = new RSA(N);
-      System.out.println(key);
- 
-      // create random message, encrypt and decrypt
-      BigInteger message = new BigInteger(N-1, random);
-
-      //// create message by converting string to integer
-      // String s = "test";
-      // byte[] bytes = s.getBytes();
-      // BigInteger message = new BigInteger(s);
-
-      BigInteger encrypt = key.encrypt(message);
-      BigInteger decrypt = key.decrypt(encrypt);
-      System.out.println("message   = " + message);
-      System.out.println("encrpyted = " + encrypt);
-      System.out.println("decrypted = " + decrypt);
-   }
-   
-   
-   public void rsaTest() throws Exception{
-// Generate new key
-KeyPair keyPair = KeyPairGenerator.getInstance("RSA").generateKeyPair();
-PrivateKey privateKey = keyPair.getPrivate();
-String plaintext = "This is the message being signed";
-
-// Compute signature
-Signature instance = Signature.getInstance("SHA1withRSA");
-instance.initSign(privateKey);
-instance.update((plaintext).getBytes());
-byte[] signature = instance.sign();
-
-// Compute digest
-MessageDigest sha1 = MessageDigest.getInstance("SHA1");
-byte[] digest = sha1.digest((plaintext).getBytes());
-
-// Encrypt digest
-Cipher cipher = Cipher.getInstance("RSA");
-cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-byte[] cipherText = cipher.doFinal(digest);
-
-// Display results
-System.out.println("Input data: " + plaintext);
-System.out.println("Digest: " + bytes2String(digest));
-System.out.println("Cipher text: " + bytes2String(cipherText));
-System.out.println("Signature: " + bytes2String(signature));
-   }
-   
-   
-   private static String bytes2String(byte[] bytes) {
-	    StringBuilder string = new StringBuilder();
-	    for (byte b : bytes) {
-	        String hexString = Integer.toHexString(0x00FF & b);
-	        string.append(hexString.length() == 1 ? "0" + hexString : hexString);
+		//Encrypt
+		byte[] cipherText = null;
+	    try {
+	      // get an RSA cipher object and print the provider
+	      final Cipher cipher = Cipher.getInstance("RSA");
+	      // encrypt the plain text using the public key
+	      cipher.init(Cipher.ENCRYPT_MODE, pubK);
+	      cipherText = cipher.doFinal(m.getBytes("UTF-8"));
+	    } catch (Exception e) {
+	      e.printStackTrace();
 	    }
-	    return string.toString();
+		
+	    
+	    
+	    //Decrypt
+	    byte[] dectyptedText = null;
+	    try {
+	      // get an RSA cipher object and print the provider
+	      final Cipher cipher = Cipher.getInstance("RSA");
+
+	      // decrypt the text using the private key
+	      cipher.init(Cipher.DECRYPT_MODE, privK);
+	      dectyptedText = cipher.doFinal(cipherText);
+
+	    } catch (Exception ex) {
+	      ex.printStackTrace();
+	    }
+		
+	    String result = new String(dectyptedText);
+	    
+	    if (!result.equals(m)){
+	    	throw new SecurityException("RSA encryption/decryption exception");
+	    }
+	}
+	
+	public static byte[] encrypt(String m, PrivateKey pK){
+		return encrypt(m.getBytes(), pK);
+	}
+	
+	public static byte[] encrypt(byte[] m, PrivateKey pK){
+		//ENCRYPT
+		byte[] result = null;
+	    try {
+	      // get an RSA cipher object and print the provider
+	      final Cipher cipher = Cipher.getInstance("RSA");
+	      // encrypt the plain text using the public key
+	      cipher.init(Cipher.ENCRYPT_MODE, pK);
+	      result = cipher.doFinal(m);
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	    
+	    return result;
+	}
+	
+	public static byte[] decrypt(String m, PublicKey pK){
+		return decrypt(m.getBytes(), pK);
+	}
+	
+	public static byte[] decrypt(byte[] m, PublicKey pK){
+		//DECRYPT
+		byte[] result = null;
+	    try {
+	      // get an RSA cipher object and print the provider
+	      final Cipher cipher = Cipher.getInstance("RSA");
+
+	      // decrypt the text using the private key
+	      cipher.init(Cipher.DECRYPT_MODE, pK);
+	      result = cipher.doFinal(m);
+
+	    } catch (Exception ex) {
+	      ex.printStackTrace();
+	    }
+	    
+	    return result;
 	}
 }
--- E-Voting/src/org/evoting/security/Security.java
+++ E-Voting/src/org/evoting/security/Security.java
@@ -1,68 +1,40 @@
 package org.evoting.security;
 
-import org.bouncycastle.crypto.engines.ElGamalEngine;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 import org.bouncycastle.crypto.params.ElGamalPrivateKeyParameters;
 import org.bouncycastle.crypto.params.ElGamalPublicKeyParameters;
 
 public class Security implements ISecurity {
 
+	public static void main(String[] paramArrayOfString) {
+		SecurityTester st = new SecurityTester();
+		st.testAll();
+	}
+
 	@Override
-	public String[] encryptElGamalToBB(String u, String p, ElGamalPublicKeyParameters pK) {
-		byte[] uBytes = u.getBytes();
-		byte[] pBytes = p.getBytes();
-		
-		ElGamalEngine engine = new ElGamalEngine();
-		engine.init(true, pK);
-		
-		uBytes = engine.processBlock(uBytes, 0, uBytes.length);
-		pBytes = engine.processBlock(pBytes, 0, pBytes.length);
-		
-		String encU = bytes2String(uBytes);
-		String encP = bytes2String(pBytes);
-		
-		return new String[]{encU, encP};
+	public byte[] encryptElGamal(String m, ElGamalPublicKeyParameters pK) {
+		return ElGamal.encrypt(m, pK);
 	}
 	
 	@Override
-	public String encryptElGamalToA(String b, ElGamalPublicKeyParameters pK) {
-		byte[] bBytes = b.getBytes();
-		
-		ElGamalEngine engine = new ElGamalEngine();
-		engine.init(true, pK);
-		
-		bBytes = engine.processBlock(bBytes, 0, bBytes.length);
-		
-		String encb = new String(bBytes);
-		
-		return encb;
+	public byte[] decryptElgamal(byte[] m, ElGamalPrivateKeyParameters pK) {
+		return ElGamal.decrypt(m, pK);
 	}
 
 	@Override
-	public String encryptSignatureToCC(String candidates) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
-	@Override
-	public String encryptResponse(String response) {
-		// TODO Auto-generated method stub
-		return null;
+	public byte[] encryptRSA(String hash, PrivateKey pK) {
+		return RSA.encrypt(hash, pK);
 	}
 
 	@Override
-	public String decryptElgamalInBB(String m, ElGamalPrivateKeyParameters pK) {
-		// TODO Auto-generated method stub
-		return null;
+	public byte[] decryptRSA(byte[] m, PublicKey pK) {
+		return RSA.decrypt(m, pK);
 	}
-	
-   private static String bytes2String(byte[] bytes) {
-	    StringBuilder string = new StringBuilder();
-	    for (byte b : bytes) {
-	        String hexString = Integer.toHexString(0x00FF & b);
-	        string.append(hexString.length() == 1 ? "0" + hexString : hexString);
-	    }
-	    return string.toString();
+
+	@Override
+	public String SHA1(String m) {
+		return SHA1.hash(m);
 	}
 }
