fd58b4baac2a6447245ca262919fab2954f0471e
Andreas Precht Poulsen <dresen_precht@hotmail.com>
2014-03-31 14:16:14 +0200

    Refactored the bulleting board + suppressed some warnings in the security package. Fix these later

---
+++ E-Voting Bulletin Board/src/org/evoting/bulletinboard/Model.java
@@ -0,0 +1,80 @@
+package org.evoting.bulletinboard;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import jolie.runtime.ByteArray;
+import jolie.runtime.Value;
+
+import org.bouncycastle.crypto.params.ElGamalParameters;
+import org.bouncycastle.crypto.params.ElGamalPublicKeyParameters;
+import org.evoting.common.EncryptedCandidateList;
+import org.evoting.database.EntityManagerUtil;
+import org.evoting.database.entities.Candidate;
+import org.evoting.database.entities.Timestamp;
+import org.evoting.database.entities.Vote;
+import org.evoting.database.repositories.CandidateRepository;
+import org.evoting.database.repositories.TimestampRepository;
+import org.evoting.database.repositories.VoteRepository;
+import org.evoting.security.Security;
+
+public class Model {
+	
+	public static void processVote(int userId, byte[] encryptedVote) {
+		EntityManager entMgr = EntityManagerUtil.getEntityManager();
+		VoteRepository vr = new VoteRepository(entMgr);
+		
+		//Begin a transaction
+		entMgr.getTransaction().begin();
+		
+		//See if the user has already voted
+		Vote vote = vr.findById(userId);
+		
+		if(vote == null) {
+			//Persist the vote if the user hasn't voted yet
+			entMgr.persist(vote);
+		} else {
+			//Update the vote if the user has voted before
+			vote.setEncryptedVote(encryptedVote);
+		}
+		//Close the connection to the persistent storage
+		entMgr.getTransaction().commit();
+		entMgr.close();
+	}
+	
+	public static EncryptedCandidateList getEncryptedCandidateList() {
+		EntityManager entMgr = EntityManagerUtil.getEntityManager();
+		entMgr.getTransaction().begin();
+		
+		CandidateRepository cRepo = new CandidateRepository(entMgr);
+		List<Candidate> candidates = cRepo.findAll();
+		
+		TimestampRepository tRepo = new TimestampRepository(entMgr);
+		Timestamp timestamp = tRepo.findTime();
+		
+		EncryptedCandidateList candidateList = new EncryptedCandidateList(candidates, timestamp.getTime());
+		
+		//Close the connection to the persistent storage
+		entMgr.getTransaction().commit();
+		entMgr.close();
+		return candidateList;
+	}
+	
+	public static void setElGamalPublicKey(Security security, Value root) {
+		ElGamalPublicKeyParameters elgamalPublicKey = security.getElgamalPublicKey();
+		ElGamalParameters elgamalParameters = elgamalPublicKey.getParameters();
+		
+		Value elgamalPublicKeyValue = root.getNewChild("elgamalPublicKey");
+		elgamalPublicKeyValue.getNewChild("y").setValue(elgamalPublicKey.getY().toString());
+		Value elgamalParametersValue = elgamalPublicKeyValue.getNewChild("parameters");
+		elgamalParametersValue.getNewChild("p").setValue(elgamalParameters.getP().toString());
+		elgamalParametersValue.getNewChild("g").setValue(elgamalParameters.getG().toString());
+		elgamalParametersValue.getNewChild("l").setValue(elgamalParameters.getL());
+	}
+	
+	public static void setRSAPublicKey(Security security, Value root) {
+		byte[] rsaPublicKey = security.getRSAPublicKeyBytes();
+		root.getNewChild("rsaPublicKey").setValue(new ByteArray(rsaPublicKey));
+	}
+}
