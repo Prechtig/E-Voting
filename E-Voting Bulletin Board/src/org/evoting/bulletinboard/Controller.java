608accfabeb818b4980c1cbfb66890f5e0c086d9
markthor <markthorh@gmail.com>
2014-03-31 11:32:43 +0200

    fixed conflicts

--- E-Voting Bulletin Board/src/org/evoting/bulletinboard/Controller.java
+++ E-Voting Bulletin Board/src/org/evoting/bulletinboard/Controller.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceException;
 
 import jolie.runtime.ByteArray;
 import jolie.runtime.JavaService;
